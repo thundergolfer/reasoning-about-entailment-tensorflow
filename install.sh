35479467876406dd7938e15ff40cd73f2c76903c
Jonathon Belotti <jbelotti@zendesk.com>
2017-04-03 21:33:32 +1000

    let install.sh script support py3.5 or py2.7. not tested yet

--- install.sh
+++ install.sh
@@ -11,15 +11,34 @@
 # * REQUIREMENTS_CONDA_FILE - List of dependencies/libs to be installed with conda
 #
 
-echo "installing for Apple Mac architecture"
+# Set Python version. Checks for whether we're in a Travis build
+PYTHON_VERSION="${TRAVIS_PYTHON_VERSION:-3.5}" # or sub in 2.7 for 3.5
 
 # install Tensorflow
 if [[ `uname` == "Darwin" ]]; then
-  CURRTF=tensorflow-0.10.0-py2-none-any.whl
-  curl https://storage.googleapis.com/tensorflow/mac/cpu/tensorflow-0.10.0-py2-none-any.whl > ./tensorflow-0.10.0-py2-none-any.whl
+  echo "installing for Apple Mac architecture"
+  if [[ $PYTHON_VERSION == "3.5" ]]; then
+    CURRTF=tensorflow-0.10.0-py3-none-any.whl
+    curl https://storage.googleapis.com/tensorflow/mac/cpu/tensorflow-0.10.0-py3-none-any.whl > ./tensorflow-0.10.0-py3-none-any.whl
+
+  elif [[ $PYTHON_VERSION == "2.7" ]]; then
+    CURRTF=tensorflow-0.10.0-py2-none-any.whl
+    curl https://storage.googleapis.com/tensorflow/mac/cpu/tensorflow-0.10.0-py2-none-any.whl > ./tensorflow-0.10.0-py2-none-any.whl
+  else
+    echo "Python version must be set to '3.5' or '2.7'"
+    exit 1
+  fi
 else
-  CURRTF=tensorflow-0.10.0-cp27-none-linux_x86_64.whl
-  curl https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-0.10.0-cp27-none-linux_x86_64.whl > ./tensorflow-0.10.0-cp27-none-linux_x86_64.whl
+  echo "installing for Linux Architecture"
+  if [[ $PYTHON_VERSION == "3.5" ]]; then
+    CURRTF=tensorflow-0.10.0-cp35-cp35m-linux_x86_64.whl
+    curl https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-0.10.0-cp35-cp35m-linux_x86_64.whl > ./tensorflow-0.10.0-cp35-cp35m-linux_x86_64.whl
+  elif [[ $PYTHON_VERSION == "2.7" ]]; then
+    CURRTF=tensorflow-0.10.0-cp27-none-linux_x86_64.whl
+    curl https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-0.10.0-cp27-none-linux_x86_64.whl > ./tensorflow-0.10.0-cp27-none-linux_x86_64.whl
+  else
+    echo "Python version must be set to '3.5' or '2.7'"
+    exit 1
 fi
 
 
@@ -30,7 +49,7 @@ $conda_command info -a
 
 # Create or replace python virtual environment
 rm -rf $VIRTUAL_ENV_PATH
-$conda_command create -y python -p $VIRTUAL_ENV_PATH --file "conda_env_spec.txt"
+$conda_command create -y python=$PYTHON_VERSION -p $VIRTUAL_ENV_PATH --file "conda_env_spec.txt"
 
 
 # Install the app and its dependencies
